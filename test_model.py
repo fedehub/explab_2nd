15242af6c69bfd4b8388c60771a447e471994a2b
fedehub <fedeunivers@gmail.com>
2022-07-07 10:12:59 +0000

    [model_viewer] launch file and test script updated

--- test_model.py
+++ test_model.py
@@ -5,4 +5,4 @@ cd ./urdf
 rosrun xacro xacro detectiBot.xacro --inorder > detectiBot.urdf
 urdf_to_graphiz detectiBot.urdf
 evince detectiBot.pdf
-roslaunch robot_modelling model_viewer.launch
+roslaunch explab_2nd model_viewer.launch
