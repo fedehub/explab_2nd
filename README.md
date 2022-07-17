74d3e6a2658914814032e602db7080bd314c632d
fedehub <fedeunivers@gmail.com>
2022-07-17 11:34:01 +0000

    move_it package updated

--- README.md
+++
@@ -1,6 +0,0 @@
-
-<img align="left" width="80" height="80" src="https://github.com/fedehub.png" alt="github icon">
-<h1> Experimental Robotics Laboratory - 2nd assignment  </h1>
-
-explab_2nd_moveit package  
-
--- test_model.py
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-clear
-cd ./urdf
-rosrun xacro xacro detectiBot.xacro --inorder > detectiBot.urdf
-urdf_to_graphiz detectiBot.urdf
-evince detectiBot.pdf
-roslaunch explab_2nd model_viewer.launch
